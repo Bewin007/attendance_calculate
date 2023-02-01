94b7d58e51b96c51d8f7266b577b6b5162fba8b1
Bewin Felix <99880789+Bewin007@users.noreply.github.com>
2023-02-01 22:34:58 +0530

    logic final

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   ChakraProvider,
   Box,
@@ -10,32 +10,58 @@ import {
   theme,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import {
+  Stack,
+  Input,
+  Button,
+} from '@chakra-ui/react'
 
 function App() {
+  const [total,setData] = useState(null)
+  const [absent, setabsent] = useState(null)
+  const [final, setfinal] = useState(null)
+  const [need ,setneed] = useState(null)
+  const [bunk, setbunk] = useState(null)
+  
+  const click=() =>{
+    setfinal((absent/total)*100)
+
+    if (final<80){
+      setneed(((80-absent)/100)-total )
+    }
+    else{
+      setbunk(((80-total)/100)*absent)
+    }
+
+  }
+
+  const Total =event=>{
+    setData(event.target.value)
+  }
+
+  const Absent = eve =>{
+    setabsent(eve.target.value)
+  }
+  
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+    <ChakraProvider theme={theme} >
+      <h1 >Attendance percent cal</h1>
+      <Stack spacing={3} m={5}>
+        <Input placeholder='Enter the total working hour' onChange={Total} size='lg' />
+        <Input placeholder='Enter the present hour' id='present_hour' onChange={Absent} size='lg' />
+        <Button colorScheme='blue' onClick={click} >Button</Button>
+        <h1>you have: {final}</h1>
+        
+        <h1>hj{need}</h1>
+        <h1>{bunk}</h1>
+       
+      </Stack>
+
+
+      
+      
     </ChakraProvider>
+    
   );
 }
 
