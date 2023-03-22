6df7136ba25e5259db6a43f877e9944e971e23f3
Bewin Felix <99880789+Bewin007@users.noreply.github.com>
2023-03-22 14:32:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   Code,
   Grid,
   theme,
+  Card,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import {
@@ -20,23 +21,29 @@ function App() {
   const [total,setData] = useState(null)
   const [absent, setabsent] = useState(null)
   const [final, setfinal] = useState(null)
-  const [need ,setneed] = useState(null)
-  const [bunk, setbunk] = useState(null)
+  const [ operation, setoperation] = useState(null)
+  const [string,setString] = useState(null)
+  const [eligible,setEligible] = useState(null)
   
   const percent=() =>{
-    
-    setfinal((absent/total)*100)
+    let temp = (Math.round((absent/total)*100))
+    setfinal(temp)
+    console.log(final)
 
-  }
-  const attend = ()=>{
-    if (final<80){
-      setneed((80- absent) * (total/100))
-      alert(need)
+    if (final>80){
+      let t = (Math.abs((80- absent) * (total/100)))
+      setString("You are eligible to write the exam." )
+      setEligible("Attendance percentage of " + final)
+      setoperation("You can bunk "+t+" of classes")
     }
     else{
-      setbunk((0.8*total)-absent)
-      alert(bunk)
+      let t =(Math.abs((0.8*total)-absent))
+      setString("You are not elgible to write exam.")
+      setEligible("Attendance percentage of " + final)
+      setoperation("You need to attend "+t+" of classes")
     }
+
+    console.log(operation)
   }
 
   const Total =event=>{
@@ -49,24 +56,19 @@ function App() {
   
   return (
     <ChakraProvider theme={theme} >
-      <h1 >Attendance percent cal</h1>
+      <Card margin={100} p={20} justifyContent={'center'} >
       <Stack spacing={3} m={5}>
         <Input placeholder='Enter the total working hour' onChange={Total} size='lg' />
         <Input placeholder='Enter the present hour' id='present_hour' onChange={Absent} size='lg' />
         <Button colorScheme='blue' onClick={percent} >percent</Button>
-        <Button colorScheme='blue' onClick={attend} >scenario</Button>
-        <h1>you have: {final}</h1>
 
-      
-       
+        <Text>{string}</Text>
+        <Text>{operation}</Text>
+        <Text>{eligible}</Text>
       </Stack>
-
-
-      
-      
+      </Card>
     </ChakraProvider>
     
   );
 }
-
 export default App;
